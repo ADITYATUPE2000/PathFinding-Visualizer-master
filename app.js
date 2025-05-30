147b1574dd6ad3ff81d8b38f7e6a3cc5514b0f8b
ADITYATUPE2000 <adityatupe2000@gmail.com>
2025-05-30 15:44:59 +0530

    changed

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = "3000";
+var port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-
-app.set('port', port);
+// Only start the server if we're not in a serverless environment
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
 
 module.exports = app;
